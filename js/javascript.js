a1368686db6f2c72208e11d0effc03fb09d77a1f
Alexander <matsibora.alexander@gmail.com>
2022-04-20 11:03:20 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,9 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12)
 
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+})
+
+osm.addTo(map)
