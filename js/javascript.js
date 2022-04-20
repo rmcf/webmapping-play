962119b903f15180795b598ad1200191ece81447
Alexander <matsibora.alexander@gmail.com>
2022-04-20 11:29:57 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -7,3 +7,13 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 })
 
 osm.addTo(map)
+
+// add geoJSON polygons layer
+async function addDistrictsGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const polygons = L.geoJson(data)
+  polygons.addTo(map)
+}
+
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
