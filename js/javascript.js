1ea6eb4760254f9ce66ff136ede642f560833612
Alexander <matsibora.alexander@gmail.com>
2022-04-27 09:34:38 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -8,12 +8,62 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 osm.addTo(map)
 
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+// add geoJSON points layer
+async function addCelltowersGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const markers = L.geoJson(data)
+  const clusters = L.markerClusterGroup()
+  clusters.addLayer(markers)
+  clusters.addTo(map)
+}
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+  layer.bindPopup(feature.properties.NIMI)
+}
+
+function getColor(property) {
+  switch (property) {
+    case 1:
+      return '#ff0000'
+    case 13:
+      return '#009933'
+    case 6:
+      return '#0000ff'
+    case 7:
+      return '#ff0066'
+    default:
+      return '#ffffff'
+  }
+}
+
+function polygonStyle(feature) {
+  return {
+    fillColor: getColor(feature.properties.OBJECTID),
+    fillOpacity: 0.5,
+    weight: 1,
+    opacity: 1,
+    color: 'grey',
+  }
+}
+
 // add geoJSON polygons layer
 async function addDistrictsGeoJson(url) {
   const response = await fetch(url)
   const data = await response.json()
-  const polygons = L.geoJson(data)
+  const polygons = L.geoJson(data, {
+    onEachFeature: popUPinfo,
+    style: polygonStyle,
+  })
   polygons.addTo(map)
 }
 
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+// default map settings
+function defaultMapSettings() {
+  map.setView([58.373523, 26.716045], 12)
+}
